6023a3b400799ecdecb62f46f4b55679568537f4
haamid10 <apdixamiid919@gmail.com>
2023-11-09 16:57:19 +0300

    added nav mobile section menu nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import {AiOutlineClone, AiOutlineMenu} from "react-icons/ai"
+import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai"
 const Navbar = () => {
     const  [IsOpen,setIsOpen ] = useState(false)
      const handleNav = () => {
@@ -15,20 +15,20 @@ const Navbar = () => {
         <li className="p-4">About</li>
        </ul>
       
-       <div className="">
-        <AiOutlineMenu size={20}/>
+       <div onClick={handleNav} className="">
+        {!IsOpen ? <AiOutlineClose size={20}/>: <AiOutlineMenu size={20}/> }
+        
        </div>
-       <div className=" fixed left-0 top-0
-        w-[60%] h-full border-r-gray-900 bg-[#000300">
+       <div className={!IsOpen ? 'fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-[#000300]': 'fixed left-[-100%]'}>
        <ul className="p-4 uppercase">
         <li className="p-4 border-b border-gray-600">Home</li>
         <li className="p-4 border-b border-gray-600">Company</li>
         <li className="p-4 border-b border-gray-600">Contact</li>
         <li className="p-4">About</li>
        </ul>
-       <div className="">
+       {/* <div className="">
         <AiOutlineMenu size={20}/>
-       </div>
+       </div> */}
        </div>
 
        {/* <AiOutlineClone size={20}/> */}
