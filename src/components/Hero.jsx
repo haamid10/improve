8d5006073c3e7118c60a8fba50953d93a742d624
haamid10 <apdixamiid919@gmail.com>
2023-11-19 14:04:28 +0300

    home done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,12 @@ const Hero = () => {
         <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center" >
             <p className=" text-[#00df9a] font-bold p-2">GROWING WITH DATA ANALYTICS</p>
             <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Grow with data</h1>
-            <div className="">
-                <p className=" md:text-5xl sm:text-4xl text-xl font-bold">Fast , Flexible financing for</p>
-                <Typed strings={ ['BTB','BTS', 'SASS']} typeSpeed={10} backSpeed={10}/>
+            <div className=" md:flex md:flex-row items-center  ">
+                <p className=" md:text-5xl sm:text-4xl text-xl font-bold ">Fast , Flexible financing for</p>
+                <Typed className=" md:text-5xl sm:text-4xl text-xl font-bold pl-2" strings={ [' BTB','BTS', 'SASS']} typeSpeed={120} backSpeed={160} loop/>
             </div>
+            <p className='mt-4 md:text-2xl text-xl font-bold text-gray-500'>Monitor your data to increase revenue for BTB , BTC & SASS platforms</p>
+            <button className=' bg-[#00df9a] rounded-md my-6 mx-auto text-black font-medium  text-xl  w-[150px]   '>start</button>
         </div>
 
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,25 @@
 import { useState } from "react"
 import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai"
 const Navbar = () => {
-    const  [IsOpen,setIsOpen ] = useState(false)
+    const  [IsOpen,setIsOpen ] = useState(true)
      const handleNav = () => {
          setIsOpen(!IsOpen)
      }
   return (
     <div className=' mx-auto px-4 flex justify-between items-center h-24 text-white'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
-      <ul className="flex">
+      <ul className="md:flex hidden ">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
         <li className="p-4">Contact</li>
         <li className="p-4">About</li>
        </ul>
       
-       <div onClick={handleNav} className="">
+       <div onClick={handleNav} className=" md:hidden flex">
         {!IsOpen ? <AiOutlineClose size={20}/>: <AiOutlineMenu size={20}/> }
         
        </div>
-       <div className={!IsOpen ? 'fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-[#000300]': 'fixed left-[-100%]'}>
+       <div className={!IsOpen ? 'fixed right-0 top-12 w-[60%] h-full border-r-gray-900 bg-[#000300]': 'fixed left-[-100%]'}>
        <ul className="p-4 uppercase">
         <li className="p-4 border-b border-gray-600">Home</li>
         <li className="p-4 border-b border-gray-600">Company</li>
