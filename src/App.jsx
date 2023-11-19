f80846b7bda554a61b547aee87174f1632342f4e
haamid10 <apdixamiid919@gmail.com>
2023-11-19 16:22:56 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import Analytics from "./components/Analytics"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Newsletter from "./components/Newsletter"
 const App = () => {
   return (
     <div className='bg-black'>
       <Navbar/>
       <Hero/>
       <Analytics/>
+      <Newsletter/>
+      <Footer/>
     </div>
   )
 }
